4d004a6dfe91c0348c8bd1217b5528e1b11f30d1
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-08 14:18:42 +0900

    applied html plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
@@ -9,7 +10,9 @@ module.exports = {
     filename: "main.js", // 빌드된 js파일 이름
     path: path.resolve(__dirname, 'dist'), // 빌드된 경로 이름
   },
-
+  plugins: [new HtmlWebpackPlugin({
+    template: "./assets/index.html",
+  })],
   module: {
     rules: [
       {
