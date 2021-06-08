bf942ac52595618eb1fb806fd89f99cc7fd3295e
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-08 22:43:57 +0900

    create innerhtml

---
+++ src/constants/innerHTML.ts
@@ -0,0 +1,16 @@
+export const COPYRIGHT_INNERHTML = `
+  <span class="copyright-text">
+    &copy; ${new Date().getFullYear()} , developed 
+    ❤️
+    by
+    <a
+      href="https://github.com/loquemedalagana"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Mrs. Liberty
+    </a>
+    ${" "}
+    for a better world.
+  </span>
+`;
