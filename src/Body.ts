336c3a02d3b50776a64f9759b2ca5644eeebfdab
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-09 13:10:53 +0900

    applied card wrapper list element

--- src/Body.ts
+++ src/Body.ts
@@ -1,10 +1,13 @@
+import { CardWrapper } from './components/CardWrapper';
+
 export class Body {
   private $element: HTMLElement;
+  private $cardWrapper: CardWrapper;
 
   constructor() {
     this.$element = document.querySelector('main');
+    this.$cardWrapper = new CardWrapper();
     this.$element.classList.add('container', 'container-fluid');
+    this.$cardWrapper.attachTo(this.$element);
   }
-
-  // add card elements
 }
