2c6012536a361f181d17db907e826aa75eff367c
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-09 12:08:40 +0900

    applied navigation component

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,17 +1,21 @@
+import { Navigation } from "./Navigation";
 import { APP_TITLE_INNTERHTML } from '../constants/innerHTML';
 
 export class Header {
   private $headerWrapper: HTMLElement;
   private $headerContainer: HTMLElement;
+  private $navigation: Navigation;
 
   constructor() {
     this.$headerWrapper = document.createElement('header');
     this.$headerContainer = document.createElement('div');
+    this.$navigation = new Navigation();
 
     // setAttribute vs classlist.add 차이점
     this.$headerWrapper.setAttribute('class', 'appbar appbar-main-color')
     this.$headerContainer.setAttribute('class', 'container container-fluid');
     this.$headerContainer.innerHTML = APP_TITLE_INNTERHTML;
+    this.$navigation.attachNavigation(this.$headerContainer);
     this.$headerWrapper.appendChild(this.$headerContainer);
   }
 
