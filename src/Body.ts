9ec78b9ae6975755d1dab5d7c531dbea36de0ada
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-09 10:57:37 +0900

    edited body component

--- src/Body.ts
+++ src/Body.ts
@@ -1,9 +1,7 @@
 export class Body {
-  $app: HTMLElement;
   $main: HTMLElement;
 
   constructor() {
-    this.$app = document.querySelector('body');
     this.$main = document.getElementById('app');
     this.$main.classList.add('container', 'container-fluid');
   }
