cf92ce5d46ec6ace075068de83638729a4377281
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-09 13:33:44 +0900

    edited image component

--- src/components/Image.ts
+++ src/components/Image.ts
@@ -1,16 +1,15 @@
 import { ImageItem } from '../types/item';
+import { IMAGE_THUMBNAIL_INNERHTML } from '../constants/innerHTML';
 
 export class Image {
   private $element: HTMLImageElement | HTMLElement;
 
   constructor(imageInfo: ImageItem) {
-    this.$element = document.createElement('image');
-    this.$element.setAttribute('alt', imageInfo.title);
-    this.$element.setAttribute(`id`, `image-${imageInfo.id}`);
-    this.$element.setAttribute('src', imageInfo.url);
+    this.$element = document.createElement('template');
+    this.$element.innerHTML = IMAGE_THUMBNAIL_INNERHTML(imageInfo);
   }
 
-  addImage(parent: HTMLElement, position: InsertPosition = "afterbegin") {
+  addImage(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     parent.insertAdjacentElement(position, this.$element);
   }
 }
