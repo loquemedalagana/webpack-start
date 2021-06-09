7c6cab0239591c996ebc9e43987038a848674d9e
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-09 10:44:34 +0900

    update header component

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,15 +1,21 @@
-import {Core} from "../Core";
-import { APP_TITLE } from '../constants/texts';
+import { Core } from '../Core';
+import { APP_TITLE_INNTERHTML } from '../constants/innerHTML';
 
 export class Header extends Core {
   $headerWrapper: HTMLElement;
+  $headerContainer: HTMLElement;
+
   constructor() {
     super();
     this.$headerWrapper = document.createElement('header');
-
+    this.$headerContainer = document.createElement('div');
+    this.$headerContainer.classList.add('container container-fluid');
+    this.$headerContainer.innerHTML = APP_TITLE_INNTERHTML;
+    this.$headerWrapper.appendChild(this.$headerContainer);
   }
 
   createHeader() {
 
+    this.$app.prepend(this.$headerWrapper);
   }
 }
