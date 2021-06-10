0d0fcb29a0a65b536e154b5eeeb54d60243b49d8
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-10 22:42:47 +0900

    create card component

--- src/components/Card.ts
+++ src/components/Card.ts
@@ -1,9 +1,20 @@
-import { Core } from '../util/Core';
+import { Component, Core } from '../util/Core';
 import { CARD_INNERHTML } from '../constants/innerHTML';
-import { ImageItem, VideoItem, PostItem } from "../types/item";
 
 export class Card extends Core<HTMLElement> {
-  constructor(postData: ImageItem | VideoItem | PostItem) {
+  constructor() {
     super(CARD_INNERHTML);
+  }
+
+  addChild(child: Component, type?: 'media' | 'description' | 'header') {
+    // 참고: Core.ts
+    const root = this.$element.querySelector('.card-root') as HTMLElement;
+    const container = this.$element.querySelector('.card-body') as HTMLElement;
+
+    if (type === 'header') {
+      child.attachTo(root, 'afterbegin');
+    } else {
+      child.attachTo(container, type === 'media' ? 'afterbegin' : 'beforeend');
+    }
   }
 }
