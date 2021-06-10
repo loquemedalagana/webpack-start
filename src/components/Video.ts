2e9e5977e9bdae0329fc9adbb2fc19d8e8e0c41c
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-10 11:01:30 +0900

    applied url

--- src/components/Video.ts
+++ src/components/Video.ts
@@ -3,11 +3,14 @@ import { VideoItem } from '../types/item';
 import { VIDEO_INNERHTML } from '../constants/innerHTML';
 
 export class Video extends Core<HTMLIFrameElement | HTMLElement>{
+  videoURL: string;
+
   constructor(videoInfo: VideoItem) {
     super(VIDEO_INNERHTML);
+    this.videoURL = `https://www.youtube.com/embed/${videoInfo.id}`;
 
     const $videoElement = this.$element.querySelector('video-player')! as HTMLElement; // changed to iframe element
-    $videoElement.innerText = videoInfo.videoId;
+    $videoElement.innerText = videoInfo.id;
 
     const $descriptionElement = this.$element.querySelector('.card-description')! as HTMLElement;
     $descriptionElement.innerText = videoInfo.description;
