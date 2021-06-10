2d6b9deb9fda8e332173b2ce0b7fe1d457f141ab
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-10 19:49:49 +0900

    set extract data from local storage

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
-import { Header } from "./components/Header";
+import { Header } from './components/Header';
 import { Body } from './Body';
 import { Footer } from './components/Footer';
+import { samplePostList } from './data/sampleData';
 
 export class App {
   private $header: Header;
@@ -11,8 +12,18 @@ export class App {
     this.$header = new Header();
     this.$body = new Body();
     this.$footer = new Footer();
-    this.$header.attachTo(appRoot, "afterbegin");
-    this.$footer.attachTo(appRoot, "beforeend");
+    this.$header.attachTo(appRoot, 'afterbegin');
+    this.$footer.attachTo(appRoot, 'beforeend');
+
+    this.setItemList();
   }
 
+  private setItemList() {
+    samplePostList.forEach(samplePost => {
+      const foundValue = localStorage.getItem(samplePost.id);
+      if(foundValue === null) {
+        localStorage.setItem(samplePost.id, JSON.stringify(samplePost));
+      }
+    });
+  }
 }
