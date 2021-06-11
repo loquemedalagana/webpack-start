104fcc22696e45e865be6f79758aa83a39d960a2
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-11 10:13:49 +0900

    added event listener

--- src/components/CardHeader.ts
+++ src/components/CardHeader.ts
@@ -1,11 +1,23 @@
 import { Core } from '../util/Core';
 import { CARD_HEADER_INNERHTML } from '../constants/innerHTML';
 
+type OnCloseLister = () => void;
+
 export class CardHeader extends Core<HTMLElement> {
+  private handleClose?: OnCloseLister;
+
   constructor(id: string, title?: string) {
     super(CARD_HEADER_INNERHTML);
     const $cardTitle = this.$element.querySelector('.card-title')! as HTMLHeadingElement;
     const $closeButton = this.$element.querySelector('.card-close-button')! as HTMLButtonElement;
     $cardTitle.innerText = title || "제목 없음";
+
+    $closeButton.onclick = () => {
+      this.handleClose && this.handleClose();
+    };
+  }
+
+  setOnCloseLister(listener: OnCloseLister) {
+    this.handleClose = listener;
   }
 }
