92360484782587cb283ab9cb8b1693716d888b43
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-12 23:55:28 +0900

    fix input type

--- src/types/item.d.ts
+++ src/types/item.d.ts
@@ -1,7 +1,7 @@
 export type PostType = "post" | "image" | "video";
 export type ComponentType = 'media' | 'description' | 'header' | 'footer' | 'modal' | 'form' | 'input';
 export type ModalType = 'view-post-detail' | 'add-card';
-export type InputType = 'image-url-input' | 'video-url-input' | 'title-input' | 'description-input';
+export type InputType = 'url-input' | 'title-input' | 'description-input';
 
 export type PostItem = {
   type: PostType;
