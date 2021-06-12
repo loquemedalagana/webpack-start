6174f5e262b51aaf2dbbf53f98ba0173fb4b9731
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-12 14:00:06 +0900

    added description type

--- src/components/Card/CardDescription.ts
+++ src/components/Card/CardDescription.ts
@@ -3,7 +3,7 @@ import { CARD_DESCRIPTION_INNERHTML } from '../../constants/innerHTML';
 
 export class CardDescription extends Core<HTMLElement> {
   constructor(description?: string) {
-    super(CARD_DESCRIPTION_INNERHTML);
+    super(CARD_DESCRIPTION_INNERHTML, 'description');
 
     const $descriptionElement = this.$element.querySelector('.card-description')! as HTMLElement;
     $descriptionElement.innerText = description || "해당 포스트에는 내용이 없습니다.";
