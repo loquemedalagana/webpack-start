7c977b3cbe6e93f021a3f3e3906a5c34035035b3
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-12 23:53:05 +0900

    edited modal form

--- src/components/Modal/ModalForm.ts
+++ src/components/Modal/ModalForm.ts
@@ -1,9 +1,8 @@
 import { Composable, Core, Component } from "../Core";
-import { OnSubmitListener } from "../../types/eventlistener";
 import { MODAL_FORM_INNERHTML } from "../../constants/innerHTML";
 
 export class ModalForm extends Core<HTMLElement> implements Composable {
-  constructor(onSubmit?: OnSubmitListener) {
+  constructor() {
     super(MODAL_FORM_INNERHTML, 'form');
 
     const $firstInputWrapper = this.$element.firstElementChild! as HTMLElement;
