041c04796b71e7c89c9fc23fd170da75a9aee735
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-12 15:31:25 +0900

    create modal action componnet

---
+++ src/components/Modal/ModalAction.ts
@@ -0,0 +1,15 @@
+import { Core } from "../Core";
+import { MODAL_ACTION_INNERHTML } from "../../constants/innerHTML";
+import { OnSubmitListener } from "../../types/eventlistener";
+
+export class ModalAction extends Core<HTMLElement> {
+  constructor(onSubmit?: OnSubmitListener) {
+    super(MODAL_ACTION_INNERHTML, 'footer');
+    const $submitButton = this.$element.querySelector('.modal-action-button')! as HTMLButtonElement;
+    $submitButton.innerText = 'add';
+
+    $submitButton.onclick = () => {
+      onSubmit && onSubmit();
+    }
+  }
+}
