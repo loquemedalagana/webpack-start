8567ae30027b758ad183d56e4f3724e9b7339913
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-12 16:07:38 +0900

    create modal type

--- src/types/item.d.ts
+++ src/types/item.d.ts
@@ -1,5 +1,6 @@
 export type PostType = "post" | "image" | "video";
 export type ComponentType = 'media' | 'description' | 'header' | 'footer' | 'modal' | 'form';
+export type ModalType = 'view-post-detail' | 'add-card';
 
 export type PostItem = {
   type: PostType;
