6704eb21f98205c423eb54ee82eda3a0b27c89de
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-12 23:46:12 +0900

    added modal input type

--- src/components/Modal/Input.ts
+++ src/components/Modal/Input.ts
@@ -1,14 +1,11 @@
 import { Core } from "../Core";
 import { OnKeyPressListener } from "../../types/eventlistener";
 import { InputType } from "../../types/item";
+import { INPUT_INNERHTML } from "../../constants/innerHTML";
 
 export class Input extends Core<HTMLElement>{
   constructor(id: InputType, onKeyPress: OnKeyPressListener) {
-    super(`
-      <section>
-        <input>      
-      </section>
-    `);
+    super(INPUT_INNERHTML, 'input');
 
     const $inputElement = this.$element.querySelector('.input')! as HTMLInputElement;
     $inputElement.id = id;
