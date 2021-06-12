cbb4df43565058ba6cc37ac273810c92573c33f8
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-12 15:17:56 +0900

    create modal header component

---
+++ src/components/Modal/ModalHeader.ts
@@ -0,0 +1,18 @@
+import {Core} from "../Core";
+import {MODAL_HEADER_INNERHTML} from "../../constants/innerHTML";
+import {OnCloseLister} from "../../types/eventlistener";
+import { PostType } from "../../types/item";
+
+export class ModalHeader extends Core<HTMLElement> {
+  constructor(title: PostType | string, onClose: OnCloseLister) {
+    super(MODAL_HEADER_INNERHTML, 'header');
+
+    const $modalTitle = this.$element.querySelector('.modal-title')! as HTMLHeadingElement;
+    const $closeButton = this.$element.querySelector('.modal-close-button')! as HTMLButtonElement;
+    $modalTitle.innerText = title || '제목 없음';
+
+    $closeButton.onclick = () => {
+      onClose && onClose();
+    };
+  }
+}
