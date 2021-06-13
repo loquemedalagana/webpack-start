93daf3bed5f51e13dd19c16d563617533506f19e
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-13 17:42:34 +0900

    changed variable name

--- src/Body.ts
+++ src/Body.ts
@@ -4,12 +4,12 @@ import { CardHeader } from './components/Card/CardHeader';
 
 export class Body {
   private $element: HTMLElement;
-  private $cardWrapper: CardList;
+  private $cardList: CardList;
 
   constructor() {
     this.$element = document.querySelector('main');
-    this.$cardWrapper = new CardList(Card, CardHeader);
+    this.$cardList = new CardList(Card, CardHeader);
     this.$element.setAttribute('class', 'container');
-    this.$cardWrapper.attachTo(this.$element, 'afterbegin');
+    this.$cardList.attachTo(this.$element, 'afterbegin');
   }
 }
