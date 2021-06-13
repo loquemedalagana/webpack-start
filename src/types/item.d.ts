f7ef2452d15fa92eeacfbf2b3e83653db34d0618
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-13 18:21:20 +0900

    remove class

--- src/types/item.d.ts
+++ src/types/item.d.ts
@@ -1,3 +1,5 @@
+import { youtubeRegExp } from "../constants/regExp";
+
 export type PostType = "post" | "image" | "video";
 export type ComponentType = 'media' | 'description' | 'header' | 'footer' | 'modal' | 'form' | 'input';
 export type ModalType = 'view-post-detail' | 'add-card';
