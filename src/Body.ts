48c64b0a2b0fc902b1b8904bdc6e66b88b84d1d8
loquemedalagana <nadamasquetu012@gmail.com>
2021-06-13 17:43:35 +0900

    원상복귀

--- src/Body.ts
+++ src/Body.ts
@@ -1,12 +1,13 @@
 import { CardList } from './components/Card/CardList';
 import { Card } from './components/Card/Card';
 import { CardHeader } from './components/Card/CardHeader';
+import { ModalRoot } from "./components/Modal/ModalRoot";
 
 export class Body {
   private $element: HTMLElement;
   private $cardList: CardList;
 
-  constructor() {
+  constructor(modalRoot: ModalRoot) {
     this.$element = document.querySelector('main');
     this.$cardList = new CardList(Card, CardHeader);
     this.$element.setAttribute('class', 'container');
